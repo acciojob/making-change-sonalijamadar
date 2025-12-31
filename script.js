1feb0c73bdc3f89f454c5b38e1d0c7211c5651ab
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2025-12-31 11:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	c = Number (c);
+
+	let result = {
+		q : 0,
+		d : 0,
+        n : 0,
+		p : 0
+	};
+	result.q = Math.floor(c / 25);
+	c = c % 25;
+	result.d = Math.floor(c / 10);
+	c = c % 10;
+	result.n = Math.floor(c / 5);
+	c = c % 5 ;
+	result.p = c;
+	return result;
 };
 
 // Do not the change the code below
